87c325f998af11b5ea7f17d1dc36fbd9076fe29c
louisyeung <louis0yeung@gmail.com>
2019-02-13 17:12:01 -0500

    added catkin workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/catkin_ws/src
+# Build directory: /home/parallels/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(turtlebot3_simulations/turtlebot3_simulations)
+subdirs(turtlebot3_simulations/turtlebot3_fake)
+subdirs(turtlebot3_simulations/turtlebot3_gazebo)
