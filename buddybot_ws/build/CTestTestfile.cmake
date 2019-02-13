afdb99a4e6b95947d862336c65da7ea5d6c1d3e1
Nick <nac542@gmail.com>
2019-02-13 17:27:37 -0500

    adding gitignore file

---
+++ buddybot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/Documents/Capstone-Buddy-Bot/buddybot_ws/src
+# Build directory: /home/nick/Documents/Capstone-Buddy-Bot/buddybot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
