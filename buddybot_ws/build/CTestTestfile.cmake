cfd96df7ecbbb643dfee7881a79ac2337c487143
Nick <nac542@gmail.com>
2019-02-18 16:20:23 -0500

    removed build files

--- buddybot_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/Documents/Capstone-Buddy-Bot/buddybot_ws/src
-# Build directory: /home/nick/Documents/Capstone-Buddy-Bot/buddybot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
