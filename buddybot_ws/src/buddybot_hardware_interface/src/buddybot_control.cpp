430b4926993e778551378c7ff00bce5a99094f1b
njc111 <nac542@gmail.com>
2019-04-11 14:17:39 -0400

    Added arduino specific serial configuration

--- buddybot_ws/src/buddybot_hardware_interface/src/buddybot_control.cpp
+++ buddybot_ws/src/buddybot_hardware_interface/src/buddybot_control.cpp
@@ -1,19 +1,35 @@
 #include <buddybot_control/buddybot_control.h>
+#include <boost/asio.hpp>
+#include <boost/asio/serial_port.hpp>
+
 #include <iostream>
 
+using namespace boost;
+
 void buddybot_robot::moveWheels(double leftSpeed, double rightSpeed) {
-	// asio::io_service io;
-	// asio::serial_port port(io);
+
+	// Left and Right Speed come in as rad/sec in the range of 0 to 5 m/s
+
+	// The arduino motor driver accepts speeds from 0 to 100, but we dont 
+	// want to run at max speed, so a limit of 50 will be applied
+
+/*
+	asio::io_service io;
+	asio::serial_port port(io);
  
-	// port.open("COM3");
-	// port.set_option(asio::serial_port_base::baud_rate(115200));
+	port.open("/dev/ttyACM0");
+	port.set_option(asio::serial_port_base::baud_rate(57600));
  
-	// char c = 'a';
+	char toWrie [30];
+
+	// Create write string to arduino, multiply speed by 10 and convert to int
+	int n = sprintf (toWrite, "[%d,%d]\n", (int) (10* leftSpeed), (int) (10* rightSpeed));
  
-	// // Read 1 character into c, this will block
-	// // forever if no character arrives.
-	// asio::write(port, asio::buffer(&c,1));
+	// Read 1 character into c, this will block
+	// forever if no character arrives.
+	asio::write(port, asio::buffer(&toWrite, n));
  
-	// port.close();
+	port.close();
+*/
 	std::cout << "I CALLED MOVE WHEELS FUNCTION" << std::endl;
 }
