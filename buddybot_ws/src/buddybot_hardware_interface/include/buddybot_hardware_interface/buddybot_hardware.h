b00360d6fb454fad5009e5bb278331b73b88b309
Nick <nac542@gmail.com>
2019-04-15 22:34:37 -0400

    hardware interface works now

--- buddybot_ws/src/buddybot_hardware_interface/include/buddybot_hardware_interface/buddybot_hardware.h
+++ buddybot_ws/src/buddybot_hardware_interface/include/buddybot_hardware_interface/buddybot_hardware.h
@@ -1,57 +1,110 @@
 #ifndef ROS_CONTROL__ROBOT_HARDWARE_H
 #define ROS_CONTROL__ROBOT_HARDWARE_H
 
-#include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
-#include <joint_limits_interface/joint_limits.h>
-#include <joint_limits_interface/joint_limits_interface.h>
-#include <joint_limits_interface/joint_limits_rosparam.h>
-#include <joint_limits_interface/joint_limits_urdf.h>
 #include <controller_manager/controller_manager.h>
-#include <boost/scoped_ptr.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/asio/serial_port.hpp>
+
+#include <ros/console.h>
+using namespace boost;
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 #include <ros/ros.h>
 
-namespace buddybot_hardware_interface
+/// \brief Hardware interface for a robot
+class buddybotHardware : public hardware_interface::RobotHW 
 {
-    /// \brief Hardware interface for a robot
-    class buddybotHardware : public hardware_interface::RobotHW 
-    {
-        protected:
-            // Interfaces
-            hardware_interface::JointStateInterface joint_state_interface_;
-            hardware_interface::PositionJointInterface position_joint_interface_;
-            hardware_interface::VelocityJointInterface velocity_joint_interface_;
-            hardware_interface::EffortJointInterface effort_joint_interface_;
+    public:
+        buddybotHardware(){
 
-            joint_limits_interface::EffortJointSaturationInterface effort_joint_saturation_interface_;
-            joint_limits_interface::EffortJointSoftLimitsInterface effort_joint_limits_interface_;
-            joint_limits_interface::PositionJointSaturationInterface position_joint_saturation_interface_;
-            joint_limits_interface::PositionJointSoftLimitsInterface position_joint_limits_interface_;
-            joint_limits_interface::VelocityJointSaturationInterface velocity_joint_saturation_interface_;
-            joint_limits_interface::VelocityJointSoftLimitsInterface velocity_joint_limits_interface_;
+            // Resize vectors
+            joint_position_[0] = 0;
+            joint_velocity_[0] = 0;
+            joint_effort_[0] = 0;
+            joint_velocity_command_[0] = 0;
+            joint_position_[1] = 0;
+            joint_velocity_[1] = 0;
+            joint_effort_[1] = 0;
+            joint_velocity_command_[1] = 0;
 
-            // Custom or available transmissions
-            // transmission_interface::RRBOTTransmission rrbot_trans_;
-            // std::vector<transmission_interface::SimpleTransmission> simple_trans_;
+             // Create joint state interface
+            hardware_interface::JointStateHandle jointStateHandle_0("left_wheel_joint", &joint_position_[0], &joint_velocity_[0], &joint_effort_[0]);
+            joint_state_interface_.registerHandle(jointStateHandle_0);
+            hardware_interface::JointStateHandle jointStateHandle_1("right_wheel_joint", &joint_position_[1], &joint_velocity_[1], &joint_effort_[1]);
+            joint_state_interface_.registerHandle(jointStateHandle_1);
 
-            // Shared memory
-            int num_joints_;
-            int joint_mode_; // position, velocity, or effort
-            std::vector<std::string> joint_names_;
-            std::vector<int> joint_types_;
-            std::vector<double> joint_position_;
-            std::vector<double> joint_velocity_;
-            std::vector<double> joint_effort_;
-            std::vector<double> joint_position_command_;
-            std::vector<double> joint_velocity_command_;
-            std::vector<double> joint_effort_command_;
-            std::vector<double> joint_lower_limits_;
-            std::vector<double> joint_upper_limits_;
-            std::vector<double> joint_effort_limits_;
 
-    }; // class
 
-} // namespace
+            // Create velocity joint interface
+            hardware_interface::JointHandle jointVelocityHandle_0(joint_state_interface_.getHandle("left_wheel_joint"), &joint_velocity_command_[0]);
+            hardware_interface::JointHandle jointVelocityHandle_1(joint_state_interface_.getHandle("right_wheel_joint"), &joint_velocity_command_[1]);
+            registerInterface(&joint_state_interface_);
+
+            velocity_joint_interface_.registerHandle(jointVelocityHandle_0);
+            velocity_joint_interface_.registerHandle(jointVelocityHandle_1);
+
+            registerInterface(&velocity_joint_interface_);
+        }
+        ~buddybotHardware() { }
+        void read(){ }
+        void write() {
+            // Left and Right Speed come in as rad/sec in the range of 0 to 5 m/s
+
+            // The arduino motor driver accepts speeds from 0 to 100, but we dont 
+            // want to run at max speed, so a limit of 50 will be applied
+
+            /*
+                asio::io_service io;
+                asio::serial_port port(io);
+             
+                port.open("/dev/ttyACM0");
+                port.set_option(asio::serial_port_base::baud_rate(57600));
+             
+                char toWrie [30];
+
+                // Create write string to arduino, multiply speed by 10 and convert to int
+                int n = sprintf (toWrite, "[%d,%d]\n", (int) (10* leftSpeed), (int) (10* rightSpeed));
+             
+                // Read 1 character into c, this will block
+                // forever if no character arrives.
+                asio::write(port, asio::buffer(&toWrite, n));
+             
+                port.close();
+            */
+            double leftSpeed = joint_velocity_command_[0];
+            double rightSpeed = joint_velocity_command_[1];
+
+            char toWrite [100];
+
+            // Create write string to arduino, multiply speed by 10 and convert to int
+            int n = std::sprintf(toWrite, "[%f,%f]\n", leftSpeed, rightSpeed);
+
+            ofstream myfile;
+            myfile.open ("/home/nick/Documents/Capstone-Buddy-Bot/buddybot_ws/src/buddybot_hardware_interface/src/test.txt");
+            myfile << toWrite << std::endl;
+            myfile.close();
+        }
+
+    protected:
+        // Interfaces
+        hardware_interface::JointStateInterface joint_state_interface_;
+        hardware_interface::VelocityJointInterface velocity_joint_interface_;
+
+        // Shared memory
+        double joint_position_[2];
+        double joint_velocity_[2];
+        double joint_effort_[2];
+        double joint_velocity_command_[2];
+
+
+}; // class
 
 #endif
--- buddybot_ws/src/buddybot_hardware_interface/src/buddybot_hardware_interface_node.cpp
+++ buddybot_ws/src/buddybot_hardware_interface/src/buddybot_hardware_interface_node.cpp
@@ -1,13 +1,32 @@
-#include <buddybot_hardware_interface/buddybot_hardware_interface.h>
+#include <buddybot_hardware_interface/buddybot_hardware.h>
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "buddybot_hardware_interface");
-    ros::NodeHandle nh;
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-    buddybot_hardware_interface::buddybotHardwareInterface buddybot(nh);
-    ros::spin();
-    ROS_DEBUG("THIS TEST WORKED");
-    return 0;
+
+	ros::init(argc, argv, "buddybot_hardware_interface");
+	ros::NodeHandle nh;
+
+	buddybotHardware buddybot;
+	controller_manager::ControllerManager cm(&buddybot, nh);
+
+
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+
+	ros::Time ts = ros::Time::now();
+
+	ros::Rate rate(50);
+	while (ros::ok())
+	{
+	 ros::Duration d = ros::Time::now() - ts;
+	 ts = ros::Time::now();
+	 buddybot.read();
+	 cm.update(ts, d);
+	 buddybot.write();
+	 rate.sleep();
+	}
+
+	spinner.stop();
+
+	return 0;
 }
