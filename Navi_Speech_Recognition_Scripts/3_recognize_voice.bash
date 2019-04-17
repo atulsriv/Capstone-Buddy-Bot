e872243407d3dc8612100caf584fb780f22f5cdf
louisyeung <louis0yeung@gmail.com>
2019-04-17 00:30:54 -0400

    added scripts for running speech recognition

---
+++ Navi_Speech_Recognition_Scripts/3_recognize_voice.bash
@@ -0,0 +1,5 @@
+cd ../buddybot_ws/
+source devel/setup.bash
+cd src
+
+roslaunch navi_speech kws.launch dict:=/Capstone-Buddy-Bot/buddybot_ws/src/navi_speech/demo/voice_cmd.dic kws:=/Capstone-Buddy-Bot/buddybot_ws/src/navi_speech/demo/voice_cmd.kwlist hmm:=/Capstone-Buddy-Bot/buddybot_ws/src/navi_speech/demo/hub4wsj_sc_8k
