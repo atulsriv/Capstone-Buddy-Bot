1a2bf56d11b452486e9debe3d5d55590fd6031c6
louisyeung <louis0yeung@gmail.com>
2019-04-19 13:11:42 -0400

    added hector slam and new rplidar launch files

--- buddybot_ws/src/rplidar_ros/sdk/include/rplidar.h
+++ buddybot_ws/src/rplidar_ros/sdk/include/rplidar.h
@@ -3,7 +3,7 @@
  *
  *  Copyright (c) 2009 - 2014 RoboPeak Team
  *  http://www.robopeak.com
- *  Copyright (c) 2014 - 2018 Shanghai Slamtec Co., Ltd.
+ *  Copyright (c) 2014 - 2016 Shanghai Slamtec Co., Ltd.
  *  http://www.slamtec.com
  *
  */
@@ -34,11 +34,10 @@
 
 #pragma once
 
-#include <vector>
-#include "hal/types.h"
+#include "rptypes.h"
 #include "rplidar_protocol.h"
 #include "rplidar_cmd.h"
 
 #include "rplidar_driver.h"
 
-#define RPLIDAR_SDK_VERSION  "1.9.0"
+#define RPLIDAR_SDK_VERSION  "1.5.7"
